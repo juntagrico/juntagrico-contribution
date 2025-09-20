5e0880fe894b7bc496a4b6d0bcf91fbb9584c3c3
pascalfree <pascal.free@ihdg.ch>
2025-09-20 20:07:45 +0200

    admin list: show subscription that didn't bid yet

--- juntagrico_contribution/views/admin.py
+++ juntagrico_contribution/views/admin.py
@@ -1,8 +1,9 @@
+from django.db.models import Prefetch
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 
 from juntagrico_contribution.forms import RoundForm
-from juntagrico_contribution.models import ContributionRound
+from juntagrico_contribution.models import ContributionRound, ContributionSelection
 
 
 @permission_required('juntagrico.can_view_contributionrounds')
@@ -31,5 +32,11 @@ def details(request):
     return render(request, 'jcr/management/details.html', {
         'round': contribution_round,
         'round_form': round_form,
-        'object_list': contribution_round.selections.all(),
+        'object_list': contribution_round.subscriptions().prefetch_related(
+            Prefetch(
+                'contributions',
+                queryset=ContributionSelection.objects.filter(round=contribution_round),
+                to_attr='current_contribution'
+            )
+        ),
     })
